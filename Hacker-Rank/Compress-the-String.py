bb41a42c7b0d1c6901d1f469887cbe6fc8173e6b
Mahmoud Abdullah Saied Bebars <54509170+MahmoudBebars99@users.noreply.github.com>
2021-03-30 04:20:55 +0200

    Delete Hacker-Rank directory

--- Hacker-Rank/Compress-the-String.py
+++
@@ -1,2 +0,0 @@
-from itertools import groupby
-print(*[(len(list(p[1])), int(p[0])) for p in groupby(input())])
--- Hacker-Rank/Traingle-Quest-2.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-for i in range(1, n+1):
-    print(int((1+10**i*(-2+10**i)//81)))
