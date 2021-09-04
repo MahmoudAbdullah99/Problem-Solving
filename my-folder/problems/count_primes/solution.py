e9a58ef5a19bfcb736079cdacb24e6b498fe1e94
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2021-09-04 20:49:00 +0000

    Sync LeetCode submission - Count Primes (python3)

---
+++ my-folder/problems/count_primes/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 1:
+            return 0
+        
+        prime_list = [False, False] + [True] * (n-2)
+        
+        for i in range(2,ceil(n**0.5)):
+            if not prime_list[i]: continue
+                
+            for j in range(i*i, n, i):
+                prime_list[j] = False
+        
+        return sum(prime_list)
+            
