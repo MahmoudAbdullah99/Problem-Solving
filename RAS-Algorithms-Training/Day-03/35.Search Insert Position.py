166fcef2a90c269312be8290c701bfa8ddbaa291
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2021-09-13 05:59:43 +0200

    Uploaded Day-05 problems

--- RAS-Algorithms-Training/Day-03/35.Search Insert Position.py
+++ RAS-Algorithms-Training/Day-03/35.Search Insert Position.py
@@ -1,25 +1,29 @@
 """
 Problem Description:
-    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
-    return the index where it would be if it were inserted in order.
+    Given a sorted array of distinct integers and a target value, return the
+    index if the target is found. If not, return the index where it would be
+    if it were inserted in order.
 
     You must write an algorithm with O(log n) runtime complexity.
 
 Notes:
     - The array is already sorted in a non-increasing order.
-    - The problem is similar to the binary search approach, but instead of returning -1 if you didn't find the target,
-    you return the index where it should be
+    - The problem is similar to the binary search approach, but instead of
+    returning -1 if you didn't find the target, you return the index where it
+    should be.
 
 Steps:
-    - Search using the binary search approach, and return the index of the target if exists.
-    - If you did not find the target and it is larger than the last middle number in the list(the last indexed),
-    return its index+1.
-    - If you did not find the target and it is smaller than the last middle number in the list(the last indexed),
-    return its index.
+    - Search using the binary search approach, and return the index of the
+    target if exists.
+    - If you did not find the target and it is larger than the last middle
+    number in the list(the last indexed), return its index+1.
+    - If you did not find the target and it is smaller than the last middle
+    number in the list(the last indexed), return its index.
 
 TODO:
     - Review.
 """
+
 
 from typing import List
 
