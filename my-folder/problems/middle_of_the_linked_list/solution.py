173216d20bb4fddcafeef230c224dab8c53d7f2a
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2021-09-22 04:41:48 +0000

    Sync LeetCode submission - Middle of the Linked List (python)

---
+++ my-folder/problems/middle_of_the_linked_list/solution.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pointer_1 = head
+        pointer_2 = head
+        while pointer_1 and pointer_1.next:
+            pointer_2 = pointer_2.next
+            pointer_1 = pointer_1.next.next
+        
+        return pointer_2
+        
