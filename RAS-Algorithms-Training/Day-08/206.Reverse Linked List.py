172f1ddf15af22abc43a4c347d73d2699e9f5c04
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2021-09-27 07:02:11 +0200

    Uploaded Day-08 problem

--- RAS-Algorithms-Training/Day-08/206.Reverse Linked List.py
+++ RAS-Algorithms-Training/Day-08/206.Reverse Linked List.py
@@ -17,10 +17,12 @@ from typing import ListNode, Optional
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
