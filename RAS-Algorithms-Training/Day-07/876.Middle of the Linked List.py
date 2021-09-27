c9fa13d21c78cd362937b73b5bfe28754ea48c91
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2021-09-27 07:02:48 +0200

    Uploaded Day-07 problem

--- RAS-Algorithms-Training/Day-07/876.Middle of the Linked List.py
+++ RAS-Algorithms-Training/Day-07/876.Middle of the Linked List.py
@@ -21,10 +21,12 @@ from typing import ListNode, Optional
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast = head
