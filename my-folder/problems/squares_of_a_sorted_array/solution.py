367e46d3ce6f07a73511b00be68815c7c58bb446
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-09 03:37:09 +0000

    Sync LeetCode submission - Squares of a Sorted Array (python)

--- my-folder/problems/squares_of_a_sorted_array/solution.py
+++ my-folder/problems/squares_of_a_sorted_array/solution.py
@@ -1,27 +1,52 @@
-class Solution:
+class Solution(object):
     def sortedSquares(self, nums):
-        left = []
-        right = []
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = [i**2 for i in nums]
+        nums.sort()
+        return nums
+    
+    def optimizedSortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        squares = []
+        positives = []
+        negatives = []
+        barrier = 0
 
-        for index in range(len(nums)):
-            if nums[index] < 0:
-                left.insert(0, -nums[index])
+        for indx, ele in enumerate(nums):
+            if ele < 0:
+                barrier+=1
+            else:
+                positives.append(ele**2)
 
-            if nums[index] >= 0:
-                right.extend(nums[index:])
-                break
+        for indx in range(barrier-1,-1,-1):
+            negatives.append(nums[indx]**2)
         
-        pointer = 0
-        for i in range(len(left)):
-            while pointer < len(right):
-                if left[i] <= right[pointer]:
-                    right.insert(pointer, left[i])
-                    pointer += 1
-                    break
-                pointer += 1
-            if pointer == len(right):
-                right.extend(left[i:])
-                break
+        pos_indx, neg_indx = 0, 0
 
-        return [x ** 2 for x in right]
+        while pos_indx < len(positives) and neg_indx < len(negatives):
+            if positives[pos_indx] == negatives[neg_indx]:
+                squares.extend([positives[pos_indx], negatives[neg_indx]])
+                pos_indx += 1
+                neg_indx += 1
+            elif positives[pos_indx] < negatives[neg_indx]:
+                squares.append(positives[pos_indx])
+                pos_indx += 1
+            else:
+                squares.append(negatives[neg_indx])
+                neg_indx += 1
+
+        while pos_indx < len(positives):
+            squares.append(positives[pos_indx])
+            pos_indx += 1
         
+        while neg_indx < len(negatives):
+            squares.append(negatives[neg_indx])
+            neg_indx += 1
+
+        return squares
