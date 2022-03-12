ad77b818c47144e6442a5050a4465a3a4dddbc72
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-12 23:48:30 +0000

    Sync LeetCode submission - Two Sum (python)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,9 +1,12 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for index, num in enumerate(nums):
-            com = target-num
-            if com in dict.keys():
-                return [dict[com], index]
-            else:
-                dict[num] = index
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {k: v for v, k in enumerate(nums)}
+        for indx_1 in range(len(nums)):
+            indx_2 = nums_dict.get(target - nums[indx_1])
+            if indx_2 != None and indx_1 != indx_2:
+                return indx_1, indx_2
