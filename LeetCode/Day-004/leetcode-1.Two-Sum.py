e7e11d8f26f6637eaec4930524dc89efce16962d
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-12 23:30:02 +0200

    optimized solution for two sums problem using hash table

--- LeetCode/Day-004/leetcode-1.Two-Sum.py
+++ LeetCode/Day-004/leetcode-1.Two-Sum.py
@@ -1,3 +1,18 @@
+"""
+Problem Description:
+    - Given an array of integers nums and an integer target, return indices of
+    the two numbers such that they add up to target.
+    - You may assume that each input would have exactly one solution, and you
+    may not use the same element twice.
+    - You can return the answer in any order.
+
+Notes:
+    - 
+
+TODO:
+    - Review.
+"""
+
 class Solution(object):
     def naiveTwoSum(self, nums, target):
         """
@@ -9,3 +24,15 @@ class Solution(object):
             for indx_2 in range(indx_1+1, len(nums)):
                 if nums[indx_1] + nums[indx_2] == target:
                     return indx_1, indx_2
+    
+    def optimizedTwoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {k: v for v, k in enumerate(nums)}
+        for indx_1 in range(len(nums)):
+            indx_2 = nums_dict.get(target - nums[indx_1])
+            if indx_2 != None and indx_1 != indx_2:
+                return indx_1, indx_2
