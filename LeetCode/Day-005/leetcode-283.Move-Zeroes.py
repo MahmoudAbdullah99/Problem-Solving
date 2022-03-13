19ac827871a6285543a217526629d7fb8421a679
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-13 22:21:46 +0200

    Added problem info

--- LeetCode/Day-005/leetcode-283.Move-Zeroes.py
+++ LeetCode/Day-005/leetcode-283.Move-Zeroes.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def moveZeroes(self, nums):
+    def moveZeroesExternalArray(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
@@ -13,3 +13,29 @@ class Solution(object):
                 zeros_counter += 1
         
         nums[:] = temp_nums + zeros_counter*[0]
+    
+    def moveZeroesTwoPointers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        pointer1 = 0
+        pointer2 = 1
+
+        while pointer1 < len(nums) and pointer2 < len(nums):
+            if nums[pointer1] != 0:
+                pointer1 += 1
+                pointer2 += 1
+            elif nums[pointer1] == 0 and nums[pointer2] != 0:
+                nums[pointer1], nums[pointer2] = nums[pointer2], nums[pointer1]
+                pointer1 += 1
+                pointer2 += 1
+            elif nums[pointer1] == 0 and nums[pointer2] == 0:
+                pointer2 += 1
+        
+        print(nums)
+
+
+sol = Solution()
+nums = [1,2,3,0]
+sol.moveZeroesTwoPointers(nums)
