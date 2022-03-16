4024d0a9c31e60a483da2151289b2c1a5730b161
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-17 00:28:31 +0200

    Solution for 'Pascal/'s Triangle' problem

---
+++ LeetCode/Day-008/leetcode-118.Pascal-Triangle.py
@@ -0,0 +1,26 @@
+"""
+Problem Description:
+    - Given an integer numRows, return the first numRows of Pascal's triangle.
+    - In Pascal's triangle, each number is the sum of the two numbersdirectly
+    above it
+
+Notes:
+    - 
+    - 
+
+TODO:
+    - Review
+    - 
+"""
+
+class Solution(object):       
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        rows = [[1],[1,1]]
+        for i in range(1, numRows-1):
+            rows.append([1]+[rows[i][j]+rows[i][j+1] for j in range(len(rows[i])-1)] +[1])
+        
+        return rows[:numRows]
