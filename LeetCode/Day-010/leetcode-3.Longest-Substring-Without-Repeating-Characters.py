38fe4061014af4fe1ddc8373532600ba82c223b9
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-27 01:39:29 +0200

    Optimized solution for 'Longest Substring Without Repeating Characters' problem usin hash table

--- LeetCode/Day-010/leetcode-3.Longest-Substring-Without-Repeating-Characters.py
+++ LeetCode/Day-010/leetcode-3.Longest-Substring-Without-Repeating-Characters.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
+    def lengthOfLongestSubstringHashTable(self, s):
         """
         :type s: str
         :rtype: int
@@ -16,8 +16,35 @@ class Solution(object):
             else:
                 temp_length =  min(temp_length+1, (indx+1) - longest_substring[s[indx]])
                 longest_substring[s[indx]] = indx+1
+
+            longest_substring_count = max(temp_length, longest_substring_count)
+            
+        return longest_substring_count
+    
+    def lengthOfLongestSubstringHashTableOptimized(self, s):
+        """
+        :type s: str
+        :rtype: int
+        
+        """
+        longest_substring_count = 0
+        temp_length = 0
+        longest_substring = {}
+        
+        for indx in range(len(s)):
+            if not longest_substring.get(s[indx]):
+                longest_substring[s[indx]] = indx+1
+                temp_length += 1
+                
+            else:
+                if temp_length+1 < ((indx+1) - longest_substring[s[indx]]):
+                    temp_length += 1
+                else:
+                    temp_length = (indx+1) - longest_substring[s[indx]]
+                
+                longest_substring[s[indx]] = indx+1
             
             if temp_length > longest_substring_count:
                     longest_substring_count = temp_length
-                    
+            
         return longest_substring_count
