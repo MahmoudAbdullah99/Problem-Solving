ea02988d10ba319e5805b59de44a91d4fb482fe3
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-03-28 22:47:45 +0200

    Soltion for 'Permutation in String' using 26-sized Array

--- LeetCode/Day-011/leetcode-567.Permutation-in-String.py
+++ LeetCode/Day-011/leetcode-567.Permutation-in-String.py
@@ -63,3 +63,31 @@ class Solution():
             s2_count[ord(s2[i-len(s1)])] -= 1
             
         return False
+    
+    def checkInclusionArray(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s2)<len(s1):
+            return False
+        
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+        
+        for i in range(len(s1)):
+            s1_count[ord(s1[i])-97] += 1
+            s2_count[ord(s2[i])-97] += 1
+                
+        for i in range(len(s1), len(s2)+1):
+            if s1_count == s2_count:
+                return True
+            
+            if i < (len(s2)):
+                s2_count[ord(s2[i])-97] += 1
+            
+            # sliding window
+            s2_count[ord(s2[i-len(s1)])-97] -= 1
+            
+        return False
