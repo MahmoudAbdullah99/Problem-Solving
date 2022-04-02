d1ed208c76f02f5642fcc39c0e44bb6e31ddd9c0
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-04-02 00:13:03 +0000

    Sync LeetCode submission - Flood Fill (python)

--- my-folder/problems/flood_fill/solution.py
+++ my-folder/problems/flood_fill/solution.py
@@ -1,22 +1,35 @@
-class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
         currentColor = image[sr][sc]
-        rows, cols = len(image), len(image[0])
 
-        def fill(sr, sc, currentColor):
+        def fill(sr, sc):
             
-            if sr < 0 or sc < 0 or sr > rows-1 or sc > cols-1 or image[sr][sc] == newColor or image[sr][sc] != currentColor:
+            if sr < 0 or sc < 0 or sr >= len(image) or sc >= len(image[0]) or image[sr][sc] == newColor or image[sr][sc] != currentColor:
                     return
             
             image[sr][sc] = newColor
-            fill(sr-1, sc, currentColor)
-            fill(sr+1, sc, currentColor)
-            fill(sr, sc-1, currentColor)
-            fill(sr, sc+1, currentColor)
+            
+            fill(sr+1, sc)
+            fill(sr-1, sc)
+            fill(sr, sc+1)
+            fill(sr, sc-1)
         
+        if image[sr][sc] == newColor:
+            return image
         
-        fill(sr, sc, image[sr][sc])
+        fill(sr, sc)
         
         return image
-            
+
+        
+
+    
+
         
