a1ac219a098d4b5d0559aae63cdb614958c462aa
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-01-05 22:37:49 +0000

    Sync LeetCode submission - Binary Search (python)

--- my-folder/problems/binary_search/solution.py
+++ my-folder/problems/binary_search/solution.py
@@ -1,22 +1,17 @@
-class Solution(object):
-    def search(self, nums, target, left=0, right=None):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        if right == None:
-            right = len(nums) - 1
+class Solution:
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            mid_element = nums[middle]
+            
+            if  target == mid_element:
+                return middle
+            elif target > mid_element:
+                left = middle + 1
+            else:
+                right = middle - 1
         
-        if left > right:
-            return - 1
-        
-        mid_index = (left + right) // 2
-        mid = nums[mid_index]
-                
-        if mid == target:
-            return mid_index
-        elif mid > target:
-            return(self.search(nums, target, left, mid_index-1))
-        else:
-            return(self.search(nums, target, mid_index+1, right))
+        return -1
