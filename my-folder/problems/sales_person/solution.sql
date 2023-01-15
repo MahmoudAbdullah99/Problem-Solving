b17aa1389e34383b3d295a95e60051cee3c5eb53
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-01-15 22:52:34 +0000

    Sync LeetCode submission - Sales Person (mysql)

---
+++ my-folder/problems/sales_person/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT s.name
+FROM SalesPerson AS s
+LEFT JOIN (
+    SELECT o.sales_id, o.order_id
+    FROM Orders AS o
+    INNER JOIN Company AS c 
+    ON o.com_id = c.com_id
+    WHERE c.name = 'RED') AS t
+ON s.sales_id = t.sales_id
+WHERE t.order_id IS NULL
+;
