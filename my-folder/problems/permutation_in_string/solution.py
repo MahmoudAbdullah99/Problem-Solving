4452664f9ac4fd231bf34b59e4063a3f87f9c87a
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-01-21 00:43:41 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- my-folder/problems/permutation_in_string/solution.py
+++ my-folder/problems/permutation_in_string/solution.py
@@ -1,27 +1,22 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        if len(s2)<len(s1):
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
             return False
         
-        s1_count = [0] * 26
-        s2_count = [0] * 26
-        
+        s1_counter = {k:0 for k in s1+s2}
+        s2_counter = {k:0 for k in s1+s2}
+
         for i in range(len(s1)):
-            s1_count[ord(s1[i])-97] += 1
-            s2_count[ord(s2[i])-97] += 1
-                
+            s1_counter[s1[i]] += 1
+            s2_counter[s2[i]] += 1
+        
         for i in range(len(s1), len(s2)+1):
-            if s1_count == s2_count:
+            if s1_counter == s2_counter:
                 return True
             
-            if i < (len(s2)):
-                s2_count[ord(s2[i])-97] += 1
+            if i < len(s2):
+                s2_counter[s2[i]] += 1
             
-            s2_count[ord(s2[i-len(s1)])-97] -= 1
-            
+            s2_counter[s2[i-len(s1)]] -= 1
+        
         return False
