4d6b30de4f5530b9c6ed2edeb72bf811a9c67dcf
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-01-24 23:41:47 +0000

    Sync LeetCode submission - Merge Two Binary Trees (python3)

--- my-folder/problems/merge_two_binary_trees/solution.py
+++ my-folder/problems/merge_two_binary_trees/solution.py
@@ -4,23 +4,49 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+#         def new_node(node1=Null, node2=Null):
+#             value = TreeNode()
+#             if node1 and node2:
+#                 value.val = node1.val + node2.val
+#             elif node1:
+#                 value = node1
+#             elif node2:
+#                 value = node2
+#             return value
+            
+#         if not root1 and not root2:
+#             return
+        
+#         nodes = [root1, root2]
+#         root = TreeNode()
+#         i = 0
+#         while True:
+#             root1, root2 = nodes[i]
+#             root.val = new_node(root1,root2)
+#             root.left  = new_node(root1.left, root2.left)
+#             root.right = new_node(root1.right, root2.right)
+
+#             i+=1
+            
+#         return root
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        """
-        :type root1: TreeNode
-        :type root2: TreeNode
-        :rtype: TreeNode
-        """
-        def MergeTrees(t1, t2):
+        def next_node(node1:None, node2:None):
+            if not node1 and not node2:
+                return
+            if node1 and not node2:
+                return node1
+            elif node2 and not node1:
+                return node2
             
-            if (not t1):
-                return t2
-            if (not t2):
-                return t1
-            t1.val += t2.val
-            t1.left = MergeTrees(t1.left, t2.left)
-            t1.right = MergeTrees(t1.right, t2.right)
-            return t1
+            node = TreeNode()
+            node.left = next_node(node1.left, node2.left)
+            node.right = next_node(node1.right, node2.right)
+            node.val = node1.val + node2.val
+
+            return node
         
-        return MergeTrees(root1, root2)
-        
+        full_tree = next_node(root1, root2)
+        return full_tree
