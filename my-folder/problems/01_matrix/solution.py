87e17f525777a60c0189813c743760b3945570ad
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-01-26 15:37:28 +0000

    Sync LeetCode submission - 01 Matrix (python3)

--- my-folder/problems/01_matrix/solution.py
+++ my-folder/problems/01_matrix/solution.py
@@ -1,19 +1,30 @@
+import math
 class Solution:
-    def updateMatrix(self, mat):
-        queue = []
-        
-        for r in range(len(mat)):
-            for c in range(len(mat[0])):
-                if mat[r][c] == 0:
-                    queue.append((r, c))
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        zeros = []
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 0:
+                    zeros.append((i,j))
                 else:
-                    mat[r][c]= -1
-                    
+                    mat[i][j] = -1
         
-        for r, c in queue:
-            for new_r, new_c in [(r-1, c),(r, c-1),(r+1, c),(r, c+1)]:
-                if new_r >= 0 and new_r < len(mat) and new_c >= 0 and new_c < len(mat[0]) and mat[new_r][new_c] == -1:
-                    mat[new_r][new_c] = mat[r][c]+1
-                    queue.append((new_r,new_c))
-                    
+        for r, c in zeros:
+            if r > 0 and mat[r-1][c] == -1:
+                mat[r-1][c] = mat[r][c] + 1
+                zeros.append((r-1,c))
+            
+            if c > 0 and mat[r][c-1] == -1:
+                mat[r][c-1] = mat[r][c] + 1
+                zeros.append((r,c-1))
+            
+            if r+1 < len(mat) and mat[r+1][c] == -1:
+                mat[r+1][c] = mat[r][c] + 1
+                zeros.append((r+1,c))
+            
+            if c+1 < len(mat[0]) and mat[r][c+1] == -1:
+                mat[r][c+1] = mat[r][c] + 1
+                zeros.append((r,c+1))
+            
         return mat
+                
