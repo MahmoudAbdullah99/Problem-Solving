7f58d07c73f67fb1762706a3198adb3415444507
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-01-30 20:55:13 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,12 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        nums_dict = {k: v for v, k in enumerate(nums)}
-        for indx_1 in range(len(nums)):
-            indx_2 = nums_dict.get(target - nums[indx_1])
-            if indx_2 != None and indx_1 != indx_2:
-                return indx_1, indx_2
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {k: v for v,k in enumerate(nums)}
+
+        for i in range(len(nums)):
+            if hash_table.get(target-nums[i]) and i != hash_table[target-nums[i]]:
+                return i, hash_table[target-nums[i]]
